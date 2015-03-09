3f32cfd43aca5a6ce9f9fa74b60a409426ce0f34
tekekreative <tk@lmbrjck.com>
2015-03-09 15:39:24 -0400

    production query

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.listen(3000);
 
 module.exports = app;
 
-console.log('Server running at localhost:3000');
+console.log(process.env.NODE_ENV);
 
 // Connected all the loose ends by requiring the Express config module
 // and then using it to retrieve your application object instance
