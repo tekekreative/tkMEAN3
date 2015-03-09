f671409e6fcd021fd1847fa5bc34efacbd417d72
tekekreative <tk@lmbrjck.com>
2015-03-09 15:28:16 -0400

    updated prod config files

---
+++ config/env/production.js
@@ -0,0 +1,8 @@
+// Heroku configuration options
+module.exports = {
+	//mongodb URI
+	db: 'mongodb://heroku_app34732395:tb7ev26n8rmv12nnanoddgtcir@ds031647.mongolab.com:31647/heroku_app34732395',
+	// secret string to sign the session identifier
+	// make different for each env	
+	sessionSecret: 'prodTK2015'
+};
